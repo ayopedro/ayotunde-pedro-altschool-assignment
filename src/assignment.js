1d8b3375a0f6628c5a2d0a48e2454d730ebe415d
Ayotunde Pedro <93743034+ayopedro@users.noreply.github.com>
2022-07-03 11:50:21 +0000

    minor edit

--- src/assignment.js
+++ src/assignment.js
@@ -27,20 +27,8 @@ function countEvenNumbers(arrayOfNumbers) {
 }
 assignment.countEvenNumbers = countEvenNumbers;
 
-/**
- * Challenge - 3
- * 
- * Given an array of numbers representing temperatures in Celsius,
- * convert each number to Fahrenheit and return a new array containing the converted
- * temperatures in Fahrenheit. Decimal figures in the converted values in Fahrenheit should be removed.
- * E.g 51.21 should just be 51 (hint: Math.trunc(...) function)
- * If you did Challenge - 3, remove the comment in the line just after this function
- * 
- * See https://www.thoughtco.com/celcius-to-farenheit-formula-609227 for the conversion formula
- * 
- * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
- * @returns Array the converted temperatures in Fahrenheit
- */
+// Challenge - 3
+
 function celsiusToFahrenheit(arrayOfNumbers) {
   let fahrenheitArray = [];
   
